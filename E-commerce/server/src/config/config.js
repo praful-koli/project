e0033a3cf5d659517f6896cb0d938d9d9b7be183
praful-koli <prafulkoli852@gmail.com>
2026-05-30 03:37:08 +0530

    implement server setup with express and dotenv configuration

---
+++ E-commerce/server/src/config/config.js
@@ -0,0 +1,23 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const REQUIRED_KEYS = [
+  "MONGODB_URL",
+  "PORT"
+];
+
+
+for (const key of REQUIRED_KEYS) {
+  if (!process.env[key]) {
+    throw new Error(`❌ Missing required environment variable: ${key}`);
+  }
+}
+
+
+const config = {
+  MONGODB_URL: process.env.MONGODB_URL,
+  PORT: process.env.PORT || 3000 // optional default
+};
+
+export default config;
